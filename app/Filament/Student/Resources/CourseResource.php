e8c6a7e8d8bada4ac27e4cd0fcd70800d37d5d71
ibobdb <bobynugraha19@gmail.com>
2025-05-22 03:38:13 +0700

    refactor: remove edit and delete actions from course management table

--- app/Filament/Student/Resources/CourseResource.php
+++ app/Filament/Student/Resources/CourseResource.php
@@ -81,8 +81,7 @@ class CourseResource extends Resource
             ])
             ->actions([
                 Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
