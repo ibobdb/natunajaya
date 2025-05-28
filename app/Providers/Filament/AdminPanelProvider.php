294e55fc3e5e35c474cc9165e393a2bed1e74d7d
ibobdb <bobynugraha19@gmail.com>
2025-05-28 16:51:35 +0700

    fix(AdminPanelProvider): disable login for admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login(false)
             ->registration(false)
             ->colors([
                 'primary' => [
